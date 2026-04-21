15687f4ce338ed463f043106349721812964d1b1
Marcus Prado <mgprado1@my.waketech.edu>
2026-04-21 08:36:41 -0400

    added parametrize test

--- test_gradebook.py
+++ test_gradebook.py
@@ -5,3 +5,11 @@ def test_letter_grade_A():
 
 def test_letter_grade_F():
     assert letter_grade(45) == "F"
+
+@pytest.mark.parametrize("score, expected", [
+    (95, "A"),
+    (45, "F")
+])
+
+def test_letter_grade(score, expected):
+    asset letter_grade(score) == expected
