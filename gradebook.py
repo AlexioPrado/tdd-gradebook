2fc48afe00bda782d8e7f2d2d7b35c89d31140bd
Marcus Prado <mgprado1@my.waketech.edu>
2026-04-22 08:22:19 -0400

    checked for empty lists in average

--- gradebook.py
+++ gradebook.py
@@ -20,6 +20,10 @@ def is_passing(score):
     return score >= 60
 
 def average(scores):
+    if len(scores) == 0:
+        raises ValueError("Scores list cannot be empty")
+    
+
     return round(sum(scores) / len(scores), 2)
 
 def curved_score(score, bonus):
